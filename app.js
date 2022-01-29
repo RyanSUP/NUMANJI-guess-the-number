54f29f326ff919aa8e197428ba0d3511a40810f0
R Mo <morici23@gmail.com>
2022-01-28 22:57:58 -0500

    Add prevGuesses as a property of game

--- app.js
+++ app.js
@@ -11,3 +11,15 @@
      - End the gameplay.
  */
 
+const game = {
+    title: 'Guess the Number!',
+    biggestNum: 100,
+    smallestNum: 1,
+    secretNum: null,
+    prevGuesses: [],
+    play() {
+        this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    },
+}
+
