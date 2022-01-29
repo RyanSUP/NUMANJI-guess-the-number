dd620d0647962002d79b88b0f9d6e19367deda52
R Mo <morici23@gmail.com>
2022-01-28 23:34:25 -0500

    Get guess and validate input.

--- app.js
+++ app.js
@@ -21,19 +21,20 @@ const game = {
         this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     },
-    getGuess() {
-        let message = `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
-        userInput = window.prompt(message)
-        
+    getGuess() {        
         // 1) Prompt user for a number
-        
-        // 2) validate user input (this could be another function)
-        // convert input to a number
-        // while input is NaN or input number is NOT between smallestNum & biggestNum,
-        // then continue asking for input.
-        // Once an input is valid, return input.
-        // ! window.prompt will return a string. Convert it to a number before returning it!
-
+        let message = `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
+        let userInput = null
+        do {
+            userInput = window.prompt(message)
+            userInput = parseInt(userInput) // convert to number
+        } while(this.validateUserInput(userInput) === false)
+        return userInput
+    },
+    validateUserInput(input) {
+        let inputIsNumber = (typeof input === 'number')
+        let inputIsInRange = (input >= this.smallestNum && input <= this.biggestNum)
+        return (inputIsNumber && inputIsInRange) ? input : false
     },
 }
 
