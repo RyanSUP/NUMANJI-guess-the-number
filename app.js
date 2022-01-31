f5160567959dd1d9ae241bf1e0dda3618a4f47c3
R Mo <morici23@gmail.com>
2022-01-31 10:35:04 -0500

    Add some style

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 /**
- GUESS THE NUMBER! (going hard with the functional programming!)
- ## Features
-    
- - Allow the player to continually be prompted to enter their guess at the secret number until they guess correctly.
- - If the player has an incorrect guess, display an alert message that informs the player:
-     - Whether their guess is too high or too low, and…
-     - A list of all the previously guessed numbers (without showing the square brackets of an array).
- - If the player has guessed the secret number:
-     - Display an alert message that congrats the player and informs them of how many guesses they took.
-     - End the gameplay.
-     - prompt for a new game.
+ +-+-+-+-+-+-+-+
+ |N|U|M|A|N|J|I|
+ +-+-+-+-+-+-+-+
  */
 
 const game = {
@@ -136,3 +128,19 @@ AND CALLED OUT ITS NAME`
 }
 
 game.promptGameStart()
+
+
+
+/**
+ GUESS THE NUMBER! (going hard with the functional programming!)
+ ## Features
+    
+ - Allow the player to continually be prompted to enter their guess at the secret number until they guess correctly.
+ - If the player has an incorrect guess, display an alert message that informs the player:
+     - Whether their guess is too high or too low, and…
+     - A list of all the previously guessed numbers (without showing the square brackets of an array).
+ - If the player has guessed the secret number:
+     - Display an alert message that congrats the player and informs them of how many guesses they took.
+     - End the gameplay.
+     - prompt for a new game.
+ */
